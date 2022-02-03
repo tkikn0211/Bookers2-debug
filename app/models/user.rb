00133836ed6930a2ee2af461baeb86ba66804c41
EC2 Default User <ec2-user@ip-172-31-4-222.ec2.internal>
2022-02-03 08:43:23 +0000

    検索機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,18 +38,15 @@ class User < ApplicationRecord
   end
 
   #検索方法分岐
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @user = User.where("name LIkE?", "#{word}")
-    elsif search == "forward_match"
-      @user = User.where("name LIKE?", "#{word}%")
-    elsif search == "backword_match"
-      @user = User.where("name LIKE?", "%#{word}")
-    elsif search == "partial_match"
-      @user = User.where("name LIKE?", "%#{word}%")
+  def self.search_for(content, method)
+    if method == "perfect"
+      User.where(name: content)
+    elsif method == "forward"
+      User.where("name LIKE?", content+"%")
+    elsif method == "backward"
+      User.where("name LIKE?", "%"+content)
     else
-      @user = User.all
+      User.where('name LIKE?', "%"+content+"%")
     end
   end
-    
 end
