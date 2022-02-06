55e318a715f3ca1775d1669fab259467ce98d3c6
EC2 Default User <ec2-user@ip-172-31-4-222.ec2.internal>
2022-02-06 07:26:02 +0000

    コメント機能非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,21 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-      comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.new(book_comment_params)
+    @comment.user_id = current_user.id
+    @comment.book_id = @book.id
+    @comment.save
+    render 'book_comments/create'
+    #redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    book_comments = current_user.book_comments.find_by(book_id: book.id)
-    book_comments.destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @book_comments = current_user.book_comments.find_by(book_id: @book.id)
+    @book_comments.destroy
+    render 'book_comments/destroy'
+    #redirect_to request.referer
   end
 
 
